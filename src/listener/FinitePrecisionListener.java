b0e6f4b1eea538de23f49c5000b89c4ff6f3ca3c
papaiza <papaiza_12@hotmail.com>
2017-03-15 22:05:29 -0400

    Add float addition checking to listener

--- src/listener/FinitePrecisionListener.java
+++ src/listener/FinitePrecisionListener.java
@@ -1,5 +1,15 @@
 package listener;
 
-public class FinitePrecisionListener {
+import gov.nasa.jpf.ListenerAdapter;
+import gov.nasa.jpf.vm.Instruction;
+import gov.nasa.jpf.vm.ThreadInfo;
+import gov.nasa.jpf.vm.VM;
+import gov.nasa.jpf.vm.VMListener;
 
+public class FinitePrecisionListener extends ListenerAdapter implements VMListener{
+	public void executeInstruction(VM vm, ThreadInfo currentThread, Instruction instructionToExecute){
+		if(instructionToExecute.getClass().getName().equals("gov.nasa.jpf.jvm.bytecode.FADD")){
+			System.out.println("Random!");
+		}
+	}
 }
---
+++ src/main/Main.java
@@ -0,0 +1,13 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+		float f = 0.34f;
+		float g = 0.55f;
+		
+		float x = f + g;
+		System.out.println(x);
+	}
+
+}
